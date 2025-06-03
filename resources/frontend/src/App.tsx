f0a8401c41fc25ba67fefd2c9f3ddebff032cb1f
Harsha <harshachandim92@gmail.com>
2025-06-04 01:10:37 +0530

    Change table id into the uuid

--- resources/frontend/src/App.tsx
+++ resources/frontend/src/App.tsx
@@ -1,16 +1,11 @@
 import * as React from "react";
 
-
 const App: React.FC = () => {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </>
+    );
+};
 
-
-  return (
-      <>
-          <h1 className="text-3xl font-bold underline">
-              Hello world!
-          </h1>
-      </>
-  )
-}
-
-export default App
+export default App;
